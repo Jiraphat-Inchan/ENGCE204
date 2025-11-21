58e8d153776686a517cc51ddaad8eed0e7d8bd2e
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-11-21 16:53:25 +0700

    Update Lab1.2.java

--- Lab/Lab1.2.java
+++ Lab/Lab1.2.java
@@ -1,20 +1,20 @@
+package Lab1;
+
 import java.util.Scanner;
-public class Main { 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+	
+public class Lab12 {
+	
+	    public static void main(String[] args) {   
+	        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("กรุณาป้อนค่าความกว้าง (Width) ของสี่เหลี่ยมผืนผ้า (เป็นตัวเลขทศนิยม): ");
-        double width = scanner.nextDouble(); 
+	        double width = scanner.nextDouble(); 
 
-        System.out.print("กรุณาป้อนค่าความสูง (Height) ของสี่เหลี่ยมผืนผ้า (เป็นตัวเลขทศนิยม): ");
-        double height = scanner.nextDouble(); 
+	        double height = scanner.nextDouble();
 
-        double area = width * height;
+	        double area = width * height;
 
-        System.out.println("\n-------------");
-        System.out.println("พื้นที่ของสี่เหลี่ยมผืนผ้า (Area): " + area);
-        
-        scanner.close();
-    }
-
-}
+	        System.out.println(area);
+	        
+	        scanner.close();
+	    }
+	}
