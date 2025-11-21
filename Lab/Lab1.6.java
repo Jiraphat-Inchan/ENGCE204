e82204be4cc45a57476d72fe1637403230b47043
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-11-21 23:46:57 +0700

    Update Lab1.6.java

--- Lab/Lab1.6.java
+++ Lab/Lab1.6.java
@@ -2,27 +2,18 @@ package Lab1;
 
 import java.util.Scanner;
 
-public class Lab16 {
+public class Lab1_6 {
+
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 
-	        System.out.println("--- โปรแกรมสูตรคูณ ---");
-	        System.out.print("Input (ป้อนแม่สูตรคูณที่คุณต้องการ): ");
+	        System.out.println("กรุณาป้อนตัวเลขแม่สูตรคูณ:");
 
-	        int multiplier;
-	        try {
-	            multiplier = scanner.nextInt();
-	        } catch (java.util.InputMismatchException e) {
-	            System.out.println("ข้อผิดพลาด: กรุณาป้อนเฉพาะจำนวนเต็มเท่านั้น");
-	            scanner.close();
-	            return; 
-	        }
+	        int multiplier = scanner.nextInt();
 
-	        System.out.println("\nOutput:");
-	        
 	        for (int i = 1; i <= 12; i++) {
 	            int result = multiplier * i;
-	            
+
 	            System.out.println(multiplier + " x " + i + " = " + result);
 	        }
 
