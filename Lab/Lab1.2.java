8b03a333486069b900793f03006893ddf757b54e
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-11-22 13:46:06 +0700

    Update Lab1.2.java

--- Lab/Lab1.2.java
+++ Lab/Lab1.2.java
@@ -2,19 +2,20 @@ package Lab1;
 
 import java.util.Scanner;
 	
-public class Lab12 {
-	
-	    public static void main(String[] args) {   
+public class Lab1_2 {
+
+	    public static void main(String[] args) {
+
 	        Scanner scanner = new Scanner(System.in);
 
-	        double width = scanner.nextDouble(); 
+	        double width = scanner.nextDouble();
 
 	        double height = scanner.nextDouble();
 
 	        double area = width * height;
 
 	        System.out.println(area);
-	        
+
 	        scanner.close();
 	    }
 	}
