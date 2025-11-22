ed53fad9e8c855d0d362bb132a5d0931b1a3db95
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-11-22 12:48:53 +0700

    Update Lab1.3.java

--- Lab/Lab1.3.java
+++ Lab/Lab1.3.java
@@ -1,34 +1,20 @@
 package Lab1;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Lab13 {
+public class Lab1_3 {
 
 	    public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
-	        
-	        System.out.print("กรุณาป้อนจำนวนเต็ม 1 ค่า: ");
+	        Scanner input = new Scanner(System.in); 
 
-	        try {
-	            int number = scanner.nextInt();
-	            
-	            String result;
+	        int number = input.nextInt(); 
 
-	            if (number % 2 == 0) {
-	                result = "Even";
-	            } else {
-
-	                result = "Odd";
-	            }
-
-	            System.out.println("จำนวนที่ป้อน: " + number);
-	            System.out.println("ผลการตรวจสอบ: " + result);        
-
-	        } catch (InputMismatchException e) {
-	            System.out.println("\n ข้อผิดพลาด: กรุณาป้อนค่าเป็นตัวเลขจำนวนเต็มเท่านั้น ");
-	        } finally {
-	            scanner.close();
+	        if (number % 2 == 0) {
+	            System.out.println("Even"); 
+	        } else {
+	            System.out.println("Odd"); 
 	        }
+
+	        input.close(); 
 	    }
 	}
