e30e9b1a364ef4db8977589b9ae1ff15bfc53981
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-11-22 14:44:41 +0700

    Update Lab1.5.java

--- Lab/Lab1.5.java
+++ Lab/Lab1.5.java
@@ -1,33 +1,21 @@
-package Lab;
+package Lab1;
 
 import java.util.Scanner;
 
-public class Lab15 {
-	
+public class Lab1_5 {
+
 	    public static void main(String[] args) {
-	        
 	        Scanner scanner = new Scanner(System.in);
-	        
-	        System.out.println("--- เมนูกาแฟ ---");
+
 	        System.out.println("1 = Americano");
 	        System.out.println("2 = Latte");
 	        System.out.println("3 = Espresso");
 	        System.out.println("4 = Mocha");
-	        System.out.println("-----------------");
-	        System.out.print("กรุณาป้อนหมายเลขเมนู (1-4): ");
 
-	        int menuNumber = 0;
-	        
-	        if (scanner.hasNextInt()) {
-	            menuNumber = scanner.nextInt();
-	        } else {
-	           
-	            System.out.println("\n Invalid Menu: คุณต้องป้อน **ตัวเลข** เท่านั้น");
-	            scanner.close(); 
-	            return;
-	        }
+	        int menuNumber = scanner.nextInt();
 
 	        String menuName;
+
 	        switch (menuNumber) {
 	            case 1:
 	                menuName = "Americano";
@@ -42,16 +30,14 @@ public class Lab15 {
 	                menuName = "Mocha";
 	                break;
 	            default:
+	                
 	                menuName = "Invalid Menu";
 	                break;
 	        }
 
-	        if (menuName.equals("Invalid Menu")) {
-	            System.out.println("\n " + menuName + ": หมายเลขที่คุณป้อนอยู่นอกเหนือจากตัวเลือก 1-4");
-	        } else {
-	            System.out.println("\n" + menuName );
-	        }
+	        System.out.println(menuName);
 
 	        scanner.close();
 	    }
 	}
+
