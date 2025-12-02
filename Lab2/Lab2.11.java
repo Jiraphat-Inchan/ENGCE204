84aec4f2d03197745f0d2d6cf3424c473b19619c
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-12-02 23:53:11 +0700

    Update Lab2.11.java

--- Lab2/Lab2.11.java
+++ Lab2/Lab2.11.java
@@ -1,1 +1,45 @@
+package Lab;
 
+import java.util.Scanner;
+
+public class Lab2_11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String model = scanner.nextLine();
+        int initialYear = scanner.nextInt();
+        
+        int updatedYear = scanner.nextInt();
+
+        Car myCar = new Car(model, initialYear);
+
+        myCar.setYear(updatedYear);
+
+        System.out.println("Model: " + myCar.getModel());
+        System.out.println("Year: " + myCar.getYear());
+
+        scanner.close();
+    }
+}
+
+class Car {
+    private String model;
+    private int year;
+
+    public Car(String model, int year) {
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int newYear) {
+        this.year = newYear;
+    }
+}
