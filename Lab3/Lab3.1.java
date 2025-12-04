aab8098fa5ff5dd43d9775c26608e69942a289d9
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-12-04 15:51:21 +0700

    Update Lab3.1.java

--- Lab3/Lab3.1.java
+++ Lab3/Lab3.1.java
@@ -1,1 +1,29 @@
+package Lab;
 
+import java.util.Scanner;
+
+class User {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
+
+public class Lab3_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String inputUsername = scanner.nextLine();
+
+        User userObject = new User(inputUsername);
+
+        System.out.println(userObject.getUsername());
+
+        scanner.close();
+    }
+}
