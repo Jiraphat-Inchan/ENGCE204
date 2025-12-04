8deb8a4e64f4b9e1919d92d805015c4ed0527392
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-12-04 16:04:44 +0700

    Update Lab3.2.java

--- Lab3/Lab3.2.java
+++ Lab3/Lab3.2.java
@@ -1,1 +1,39 @@
+package Lab;
 
+import java.util.Scanner;
+
+class Player {
+    private int score;
+
+    public Player(int initialScore) {
+        this.score = initialScore;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore(int points) {
+        this.score += points; 
+    }
+
+}
+
+public class Lab3_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int initialScore = scanner.nextInt();
+
+        int pointsToAdd = scanner.nextInt();
+
+        Player player = new Player(initialScore);
+
+
+        player.addScore(pointsToAdd);
+
+        System.out.println(player.getScore());
+
+        scanner.close();
+    }
+}
