5f200877b189d7e0b36a1dfc94b5a0c15ca36cf7
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-12-05 12:13:01 +0700

    Create Lab3.3.java

---
+++ Lab3/Lab3.3.java
@@ -0,0 +1,42 @@
+package Lab;
+ 
+import java.util.Scanner;
+
+class User {
+    private String password;
+
+    public User(String initialPassword) {
+        this.password = initialPassword;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String newPassword) {
+        if (newPassword.length() >= 8) {
+            this.password = newPassword;
+            System.out.println("Password updated.");
+        } else {
+            System.out.println("Password is too short.");
+        }
+    }
+}
+
+public class Lab3_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String initialPassword = scanner.nextLine();
+
+        User user = new User(initialPassword);
+
+        String newPassword = scanner.nextLine();
+
+        user.setPassword(newPassword);
+
+        System.out.println(user.getPassword());
+
+        scanner.close();
+    }
+}
