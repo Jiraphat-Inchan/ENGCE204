d6ae446b20a151007db9be194fd29e971820a234
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-12-18 13:12:27 +0700

    Create Lab4.4.java

---
+++ Lab4/Lab4.4.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Location {
+    private double latitude;
+    private double longitude;
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Location(Location other) {
+        this.latitude = other.latitude;
+        this.longitude = other.longitude;
+    }
+
+    public void setLatitude(double newLat) {
+        this.latitude = newLat;
+    }
+
+    public void displayInfo() {
+        System.out.println("Lat: " + latitude + ", Lon: " + longitude);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double startLat = scanner.nextDouble();
+        double startLon = scanner.nextDouble();
+        double newLat = scanner.nextDouble();
+
+        Location loc1 = new Location(startLat, startLon);
+        Location loc2 = new Location(loc1);
+
+        loc1.setLatitude(newLat);
+
+        loc1.displayInfo();
+        loc2.displayInfo();
+        
+        scanner.close();
+    }
+}
