1fa1ca7007d9830b514a7c60a86b8511a4de7a30
Jiraphat-Inchan <yuukiigama@gmail.com>
2025-12-18 18:06:13 +0700

    Update Lab4.14.java

--- Lab4/Lab4.14.java
+++ Lab4/Lab4.14.java
@@ -1,1 +1,52 @@
+import java.util.Scanner;
 
+class ScoreBoard {
+    private String gameName;
+    private int[] scores;
+
+    public ScoreBoard(String gameName, int[] scores) {
+        this.gameName = gameName;
+        this.scores = scores; 
+    }
+
+    public ScoreBoard(ScoreBoard other) {
+        this.gameName = other.gameName;
+        this.scores = new int[other.scores.length];
+        for (int i = 0; i < other.scores.length; i++) {
+            this.scores[i] = other.scores[i];
+        }
+    }
+
+    public void displayScores() {
+        System.out.print(this.gameName + ": ");
+        for (int i = 0; i < scores.length; i++) {
+            System.out.print(scores[i] + (i < scores.length - 1 ? ", " : ""));
+        }
+        System.out.println();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        
+        int[] originalScores = new int[3];
+        for (int i = 0; i < 3; i++) {
+            originalScores[i] = sc.nextInt();
+        }
+
+        int newValue = sc.nextInt();
+
+        ScoreBoard sb1 = new ScoreBoard(name, originalScores);
+        ScoreBoard sb2 = new ScoreBoard(sb1);
+
+        originalScores[0] = newValue;
+
+        sb1.displayScores();
+        sb2.displayScores();
+
+        sc.close();
+    }
+}
